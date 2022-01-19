84a5321b70bd9cdc686f0883271f87631de35a72
unknown <gokul03062002@gmail.com>
2022-01-19 09:54:02 +0530

    four

--- date.js
+++ date.js
@@ -47,10 +47,23 @@ function getTime()
 module.exports.getRailwayTime=getRailwayTime; 
 function getRailwayTime()
 {
-    var today=new Date();
-    var currdate=today.toLocaleTimeString("en-In", {hour12:false, hour: '2-digit', minute: '2-digit' });
-   // console.log(currdate);
+//     var today=new Date();
+//     var currdate=today.toLocaleTimeString("en-In", {hour12:false, hour: '2-digit', minute: '2-digit' });
+//    // console.log(currdate);
+//    'Asia/Kolkata'
+   var date = new Date(Date.UTC(2022, 01, 19, 3, 0, 0));
 
-    return currdate;
+// an application may want to use UTC and make that visible
+// var options = { timeZone:  'Asia/Kolkata', timeZoneName: 'short' };
+// console.log(date.toLocaleTimeString('en-In', options));
+// currdate=date.toLocaleTimeString('en-In', options);
+
+var dateUTC = new Date();
+var dateUTC = dateUTC.getTime() 
+var dateIST = new Date(dateUTC);
+//date shifting for IST timezone (+5 hours and 30 minutes)
+dateIST.setHours(dateIST.getHours() + 5); 
+dateIST.setMinutes(dateIST.getMinutes() + 30);
+    return dateIST;
 
 }   
